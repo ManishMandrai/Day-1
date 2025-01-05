9090d79356aab8757c2da8252caa7c64aa61f020
ManishMandrai <manishkumarmandrai@gmail.com>
2025-01-05 16:20:35 +0530

    d

--- name.cpp
+++ name.cpp
@@ -1,23 +1,28 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     string name = "Manish";
     int age = 22;
-    float height = 6.0;
-    char grade = 'A';
+    float height = 6;
+    char grade = 65;
+    bool isStudent = false;
 
     cout << "Name: " << name << endl;
     cout << "Age : " << age << endl;
     cout << "Height : " << height << endl;
     cout << "Grade : " << grade << endl;
+    cout << "Is he a student : "<< isStudent << endl;
 
     age = 21;
     cout << "Her Age : " << age << endl;
     name = "NoOne 🥹";
     cout << "Her Name : " << name << endl;
-    height = 5.4;
-    cout << "Her Height : "<< height << endl;
+    height = 5;
+    cout << "Her Height : " << height << endl;
+    isStudent = true;
+    cout << "Is She a student : " << isStudent << endl;
 
     return 0;
 }
