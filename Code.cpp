c148e033bd240e05e2bd0b1e89a7476ba1a186d6
ManishMandrai <manishkumarmandrai@gmail.com>
2025-01-05 15:53:41 +0530

    C++

--- Code.cpp
+++ Code.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout << "Manish Kumar";
+int main()
+{
+
+    int age = 23;
+    float salery = 80000;
+    char grade = 'A';
+    string name = "Oppo";
+
+    cout << "Name : " << name << endl;
+    cout << "salery : " << salery << endl;
+    cout << "age : " << age << endl;
+    cout << "grade : " << grade << endl;
+
+    age = 30;
+    cout << "updated age" << age << endl;
+
     return 0;
 };
