396b091394bc61aecceefcc57d23187d34772d29
ManishMandrai <manishkumarmandrai@gmail.com>
2025-01-06 14:29:28 +0530

    code

--- new.cpp
+++ new.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int a = 10, b = 20;
-    bool result = (a > 5 && b < 30);
-    cout << "Logical AND (a > 5 && b < 30): "<< result <<endl;
+    bool result = (a > 5 && b > 30);
+    cout << "Logical AND (a > 5 && b < 30): " << result << endl;
 
     result = (a > 15 || b < 30);
-    cout << "logical OR (a < 15 || b < 30): " <<result <<endl;
+    cout << "Logical OR (a > 15 || b < 30): " << result << endl;
 
-    result = !(a<15);
-    cout << "Logical NOT(!(a>15)) : " << result <<endl;
+    result = !(a > 15);
+    cout << "Logical NOT (!(a > 14)) : " << result << endl;
 
-    
     return 0;
 };
